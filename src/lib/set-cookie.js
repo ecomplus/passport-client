1ecfb1f7f21351339e8b712a456829c25bd91cfa
Leonardo Matos <leomp120894@gmail.com>
2020-05-16 18:53:33 -0300

    fix(set-cookie): save cookie with sameSite=strict
    fixes 'Some cookies are misusing the recommended “sameSite“ attribute'

--- src/lib/set-cookie.js
+++ src/lib/set-cookie.js
@@ -2,5 +2,11 @@ export default (document, cname, cvalue, exdays) => {
   const d = new Date()
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
   const expires = 'expires=' + d.toUTCString()
-  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/'
+  const updatedCookie = cname + '=' + cvalue + '; ' + expires + '; path=/'
+  if (document.documentMode) {
+    // IE 8~11
+    document.cookie = updatedCookie
+  } else {
+    document.cookie = updatedCookie + '; samesite=strict'
+  }
 }
